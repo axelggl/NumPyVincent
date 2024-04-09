23b003645837ff3d6e0d316571efe77e6ab4b071
maxel <axel.maerten@ynov.com>
2024-04-09 16:34:16 +0200

    Exercice 10

--- Exercices/exo09.py
+++ Exercices/exo09.py
@@ -7,3 +7,4 @@ import numpy as np
 
 tab = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 print(tab)
+print(np.arange(1, 10).reshape(3, 3))
---
+++ Exercices/exo10.py
@@ -0,0 +1,6 @@
+# Sélectionnez l'élément "6" de la matrice ci-dessus.
+
+import numpy as np
+
+tab = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(tab[1, 2]) # 6
