259581443830c2cb7c503a010d9edc615bb398f8
maxel <axel.maerten@ynov.com>
2024-04-09 16:44:56 +0200

    Exercice 12

---
+++ Exercices/exo12.py
@@ -0,0 +1,10 @@
+# Multipliez tous les éléments de la matrice suivante par 2
+# 1 2 3
+# 4 5 6
+# 7 8 9
+
+import numpy as np
+
+tab = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+tab *= 2
+print(tab)
