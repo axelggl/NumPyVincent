11103bff3557923371fc3b44be8628e97b77f571
maxel <axel.maerten@ynov.com>
2024-04-09 15:26:47 +0200

    Exercices 04 et 05

---
+++ Exercices/exo04.py
@@ -0,0 +1,6 @@
+# Créez un tableau NumPy de taille 5 rempli de zéros.
+
+import numpy as np
+
+tab = np.zeros(5)
+print(tab)
---
+++ Exercices/exo05.py
@@ -0,0 +1,6 @@
+# Comment générer un tableau de nombres entiers de 10 à 50 ?
+
+import numpy as np
+
+tab = np.arange(10, 51)
+print(tab)
